b50f3c3a50e932a82bb136ef6c78ce177f012d1e
Максим <gmax7911@gmail.com>
2024-06-02 11:31:15 +0300

    exception package is ready

---
+++ src/exception/IncorrectArgumentException.java
@@ -0,0 +1,21 @@
+package exception;
+
+public class IncorrectArgumentException {
+
+    private final String argument;
+
+    public IncorrectArgumentException(String argument) {
+        this.argument = argument;
+    }
+
+    public String getArgument() {
+        return argument;
+    }
+
+    @Override
+    public String toString() {
+        return "IncorrectArgumentException{" +
+                "argument='" + argument + '\'' +
+                '}';
+    }
+}
---
+++ src/service/TaskService.java
@@ -0,0 +1,18 @@
+package service;
+
+import model.Task;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TaskService {
+
+    private static final Map<Integer, Task> allTasks = new HashMap<>();
+    private static final List<Task> removedTasks = new ArrayList<>();
+
+    public void addTask(Task task) {
+
+    }
+}
